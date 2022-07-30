35fc34854d812d95d0b60052c6156508af6d90b7
Tracy Derraco <ecert31@gmail.com>
2022-07-30 10:03:40 -0700

    reaction

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const {Schema,Model,Types} = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD YYYY [at] hh:mm a" ),
+        },
+        
+
+    },
+    {
+        toJSON:{
+            virtual: true,
+            getters: true
+        },
+        id:false,
+    }
+)
